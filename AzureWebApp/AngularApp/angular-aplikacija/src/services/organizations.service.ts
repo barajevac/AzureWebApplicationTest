4b6ce46375c847f6444bf25473160e926e267793
barajevac <vladanbarajevac@yahoo.com>
2020-12-04 22:05:37 +0100

    deleted comments

--- AzureWebApp/AngularApp/angular-aplikacija/src/services/organizations.service.ts
+++ AzureWebApp/AngularApp/angular-aplikacija/src/services/organizations.service.ts
@@ -8,8 +8,7 @@ import { environment } from '../environments/environment'
     providedIn: 'root',
   })
 export class OrganizatiosService {
-    // private readonly defaultProductionURL = "https://azurewebapiapplication20201128124852.azurewebsites.net/api/organization";
-    // private readonly defaultLocalURL = "http://localhost:53906/api/organization";
+   
 
     private readonly baseUrl : string = environment.baseUrl + '/organization'
 
